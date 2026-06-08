1aeb82e0b716ab475924056093817cf257361cee
MessiasAlves2006 <messiasalvesdesousa098@gmail.com>
2026-06-08 16:04:32 -0300

    arvore binaria

--- listaExercicio1.c
+++ listaExercicio1.c
@@ -1,30 +1,54 @@
-//Questão 1: R= c
-//Questão 2: R= b   
-//Questão 3: R= c
-//Questão 4: R= c
-//Questão 5: R= b
-//Questão 6: R= a
-//Questão 7: R= d
-//Questão 8: R= a
-//Questão 9: R= A arvore e uma estrutura de dados, que organizar dados de forma ordenada, fazendo com que o tempo de buscar seja diminuido significativamente
-//Questão 10: R= No raiz e o primeiro no que comeca a arvore, os nos folhas sao os nos que nao tem filhos, e os nos internos sao os nos que tem filhos, mas nao sao a raiz
-//Questão 11: R= Ancestrais sao os nos que estao acima do no em questao, e descendentes sao os nos que estao abaixo do no em questao
-//Questão 12: R= A profundidade de um no e a distancia entre o no e a raiz
-//Questão 13: R= Grau do no se refere a quantidade de filhos que o no tem, grau da arvore e a mesma coisa que o no com maior grau ou seja, o no que tem mais filhos
-//Questão 14: R= As arvores sao importantes pois se bem utilizadas, podem diminuir o tempo de busca de um elemento
-//Questão 15: R= No raiz(10), No folha(1), Grau Arvore(2) Altura(3) Nos(10, 5, 15, 3, 7, 12, 20, 1)
-                 10
-                /  \
-               /    \
-              5      15
-             / \    /  \
-            3   7  12   20
-           /
-          1
-
-//Questão 16: R= 
-def struct No{
+#include <stdlib.h>
+#include <stdio.h>
+ 
+typedef struct Arvore {
     int dado;
-    struct No *direita;
-    struct No *esquerda;
-} No;
+    struct Arvore *direita;
+    struct Arvore *esquerda;
+} Arvore;
+
+Arvore* CreateArvore() {
+    return NULL;
+}
+
+int ArvoreVazia(Arvore *raiz) {
+    return raiz == NULL;
+}
+
+void mostrarArvore(Arvore *raiz) {
+    if (!ArvoreVazia(raiz)) {
+        printf("%d ", raiz->dado);
+        mostrarArvore(raiz->esquerda);
+        mostrarArvore(raiz->direita);
+    }
+}
+
+void inserirArvore(Arvore **raiz, int valor) {
+    if (ArvoreVazia(*raiz)) {
+        *raiz = (Arvore*)malloc(sizeof(Arvore));
+        (*raiz)->dado = valor;
+        (*raiz)->direita = NULL;
+        (*raiz)->esquerda = NULL;
+    } else {
+        if (valor < (*raiz)->dado) {
+            inserirArvore(&(*raiz)->esquerda, valor);
+        } else {
+            inserirArvore(&(*raiz)->direita, valor);
+        }
+    }
+}
+
+int estaNaArvore(Arvore *raiz, int valor) {
+    if (ArvoreVazia(raiz)) {
+        return 0;
+    } else {
+        if (valor == raiz->dado) {
+            return 1;
+        } else if (valor < raiz->dado) {
+            return estahNaArvore(raiz->esquerda, valor);
+        } else {
+            return estahNaArvore(raiz->direita, valor);
+        }
+    }
+}
+
